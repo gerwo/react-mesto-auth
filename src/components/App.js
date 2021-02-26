4b7e6d62c3f67f3e4f8e44beb67a9a4d3b1918e8
Roman Grishin <r.grishin@bk.ru>
2021-02-26 12:14:23 +0300

    added Register & Login component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Header from "./Header";
+import Login from "./Login";
+import Register from "./Register";
 import Main from "./Main";
 import Footer from "./Footer";
 import ImagePopup from "./ImagePopup";
