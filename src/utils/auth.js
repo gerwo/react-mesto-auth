f66db6487ae53a8801acf08be186059d044ffdaf
Roman Grishin <r.grishin@bk.ru>
2021-03-03 16:31:55 +0300

    added auth.js

---
+++ src/utils/auth.js
@@ -0,0 +1,24 @@
+
+import {getResponseData} from './utils'
+
+export const BASE_URL = 'https://auth.nomoreparties.co';
+
+export const headers = {
+  'Content-Type': 'application/json'
+};
+
+export function registration({ email, password }){
+  return fetch(`${BASE_URL}/signup`, {
+        method : 'POST',
+        headers : "Content-Type": "application/json",
+        body : JSON.stringify({
+          "password": email,
+          "email": password
+        })
+        .then(result => getResponseData({result}))
+      })
+}
+
+export function login(){
+  
+}
