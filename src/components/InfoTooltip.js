26e46bc53118545f690d8519a6def6e2c3295cd2
gerwo <mustafa82@yandex.ru>
2021-03-10 18:15:37 +0300

    added styles for info tooltip

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -10,10 +10,10 @@ function  InfoTooltip(props) {
 
   return (
     <div ref={popup} className={`popup popup__${props.name} ${props.isOpen && 'popup_opened'}`} role="dialog" onClick={handleLayoutClick}>
-      <div className="popup__container">
+      <div className="popup__container popup__info">
         <button className="button button_type_close opacity" aria-label="Закрыть" type="button" onClick={props.onClose}></button>
-        <img src={props.image}/>
-        <p>{props.message}</p>
+        <img src={props.image} className="popup__info_image"/>
+        <p className="popup__info_message"> {props.message}</p>
       </div>
     </div>
   )
